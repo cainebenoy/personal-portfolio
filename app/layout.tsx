37d15909ddd72b3aa94638f792db609b43831202
cainebenoy <cainebenoy@gmail.com>
2026-01-25 20:48:52 +0530

    feat: update social media metadata and OG image

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,11 +33,21 @@ export const metadata: Metadata = {
     siteName: "Caine Benoy Portfolio",
     locale: "en_US",
     type: "website",
+    images: [
+      {
+        url: "/opengraph-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Caine Benoy | Portfolio",
+      },
+    ],
   },
   twitter: {
     card: "summary_large_image",
     title: "Caine Benoy | Portfolio",
     description: "Build Anything",
+    creator: "@cainebenoy",
+    images: ["/opengraph-image.png"],
   },
   robots: {
     index: true,
